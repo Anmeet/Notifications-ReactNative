538852535c023513e7e9ba44c454352379e69bad
Amit Bhandari <anmeet619@yahoo.com>
2023-01-18 20:55:15 +1100

    Added files

---
+++ App.js
@@ -0,0 +1,148 @@
+import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, View, Alert, Platform } from 'react-native'
+import * as Notifications from 'expo-notifications'
+import { useEffect } from 'react'
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+      shouldShowAlert: true,
+    }
+  },
+})
+
+const allowsNotificationsAsync = async () => {
+  const settings = await Notifications.getPermissionsAsync()
+  return (
+    settings.granted ||
+    settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+  )
+}
+
+const requestPermissionsAsync = async () => {
+  return await Notifications.requestPermissionsAsync({
+    ios: {
+      allowAlert: true,
+      allowBadge: true,
+      allowSound: true,
+      allowAnnouncements: true,
+    },
+  })
+}
+
+export default function App() {
+  const scheduleNotificationHandler = async () => {
+    const hasPushNotificationPermissionGranted =
+      await allowsNotificationsAsync()
+
+    if (!hasPushNotificationPermissionGranted) {
+      await requestPermissionsAsync()
+    }
+
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My first local notification',
+        body: 'This is th body of the notification.',
+        data: { userName: 'Max' },
+      },
+      trigger: {
+        seconds: 2,
+      },
+    })
+  }
+
+  function sendPushNotificationHandler() {
+    fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        to: '<Your Device Push Token>]',
+        title: 'Test - sent from a device!',
+        body: 'This is a test!',
+      }),
+    })
+  }
+
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log('NOTIFICATION RECEIVED')
+        console.log(notification)
+        const userName = notification.request.content.data.userName
+        console.log(userName)
+      }
+    )
+
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log('NOTIFICATION RESPONSE RECEIVED')
+        console.log(response)
+        const userName = response.notification.request.content.data.userName
+        console.log(userName)
+      }
+    )
+
+    return () => {
+      subscription1.remove()
+      subscription2.remove()
+    }
+  }, [])
+
+  useEffect(() => {
+    async function configurePushNotifications() {
+      const { status } = await Notifications.getPermissionsAsync()
+      let finalStatus = status
+      if (finalStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync()
+        finalStatus = status
+      }
+
+      if (finalStatus !== 'granted') {
+        Alert.alert(
+          'Permission required',
+          'Push notifications need the appropriate permissions.'
+        )
+        return
+      }
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync()
+      console.log('pushToken', pushTokenData)
+
+      if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT,
+        })
+      }
+    }
+
+    configurePushNotifications()
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Button
+        title='Schedule Notification'
+        onPress={scheduleNotificationHandler}
+      />
+      <Button
+        title='Send Push Notification'
+        onPress={sendPushNotificationHandler}
+      />
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
